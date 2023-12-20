d7f8a26d88bbcd7ea50698a3275c8318438700da
PremRathan2003 <premrathanaddanki7736@gmail.com>
2023-12-20 20:49:11 +0530

    Used functional components instead of Class component in react

--- src/components/BMICalculator.js
+++ src/components/BMICalculator.js
@@ -1,80 +1,99 @@
-import {Component} from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-class BMICalculator extends Component {
-  state = {
-    weight: '',
-    height: '',
-    bmi: false,
-  }
+const BMICalculator = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBMI] = useState(null);
+  const [weightError, setWeightError] = useState('');
+  const [heightError, setHeightError] = useState('');
 
-  onChangeWeight = event => {
-    this.setState({weight: event.target.value})
-  }
+  const onChangeWeight = event => {
+    const newWeight = event.target.value;
+    setWeight(newWeight);
+    setWeightError(validateWeight(newWeight));
+  };
 
-  onChangeHeight = event => {
-    this.setState({height: event.target.value})
-  }
+  const onChangeHeight = event => {
+    const newHeight = event.target.value;
+    setHeight(newHeight);
+    setHeightError(validateHeight(newHeight));
+  };
 
-  calculateBMI = () => {
-    const {weight, height} = this.state
-    const parsedWeight = parseFloat(weight)
-    const parsedHeight = parseFloat(height)
-
+  const validateWeight = weightValue => {
+    const parsedWeight = parseFloat(weightValue);
     if (
-      !Number.isNaN(parsedWeight) &&
-      !Number.isNaN(parsedHeight) &&
-      parsedWeight >= 1.0 &&
-      parsedWeight <= 300.0 &&
-      parsedHeight >= 0.1 &&
-      parsedHeight <= 3.0
+      Number.isNaN(parsedWeight) ||
+      parsedWeight < 1.0 ||
+      parsedWeight > 300.0
     ) {
-      const calculatedBMI = parsedWeight / parsedHeight ** 2
-      this.setState({bmi: calculatedBMI.toFixed(1)})
-    } else {
-      this.setState({bmi: false})
-      alert(
-        'Please enter valid Weight (1.0 - 300.0 kg) and Height (0.1 - 3.0 meters).',
-      )
+      return 'Please enter a valid Weight (1.0 - 300.0 kg).';
     }
-  }
+    return '';
+  };
 
-  render() {
-    const {weight, height, bmi} = this.state
-    return (
-      <div style={{padding: '10px'}}>
-        <label htmlFor="weight">Weight in kilograms: </label>
-        <input
-          type="number"
-          id="weight"
-          placeholder="Weight (kg)"
-          value={weight}
-          onChange={this.onChangeWeight}
-        />
-        <br />
-        <label htmlFor="height">Height in meters: </label>
-        <input
-          type="number"
-          id="height"
-          placeholder="Height (m)"
-          value={height}
-          onChange={this.onChangeHeight}
-        />
-        <br />
-        <button type="button" onClick={this.calculateBMI}>
-          Calculate BMI
-        </button>
-        {bmi && <p>Your BMI is: {bmi}</p>}
-        <br />
-        <button
-          type="button"
-          onClick={() => {
-            window.location.href = '/'
-          }}
-        >
-          Go to LandingPage
-        </button>
-      </div>
-    )
-  }
-}
-export default BMICalculator
+  const validateHeight = heightValue => {
+    const parsedHeight = parseFloat(heightValue);
+    if (
+      Number.isNaN(parsedHeight) ||
+      parsedHeight < 0.1 ||
+      parsedHeight > 3.0
+    ) {
+      return 'Please enter a valid Height (0.1 - 3.0 meters).';
+    }
+    return '';
+  };
+
+  const calculateBMI = () => {
+    const weightError = validateWeight(weight);
+    const heightError = validateHeight(height);
+
+    if (!weightError && !heightError) {
+      const parsedWeight = parseFloat(weight);
+      const parsedHeight = parseFloat(height);
+      const calculatedBMI = parsedWeight / (parsedHeight ** 2);
+      setBMI(calculatedBMI.toFixed(1));
+      setWeightError('');
+      setHeightError('');
+    } else {
+      setBMI(null);
+      setWeightError(weightError);
+      setHeightError(heightError);
+    }
+  };
+
+  return (
+    <div style={{ padding: '10px' }}>
+      <label htmlFor="weight">Weight in kilograms: </label>
+      <input
+        type="number"
+        id="weight"
+        placeholder="Weight (kg)"
+        value={weight}
+        onChange={onChangeWeight}
+      />
+      {weightError && <p style={{ color: 'red' }}>{weightError}</p>}
+      <br />
+      <label htmlFor="height">Height in meters: </label>
+      <input
+        type="number"
+        id="height"
+        placeholder="Height (m)"
+        value={height}
+        onChange={onChangeHeight}
+      />
+      {heightError && <p style={{ color: 'red' }}>{heightError}</p>}
+      <br />
+      <button type="button" onClick={calculateBMI}>
+        Calculate BMI
+      </button>
+      {bmi && <p>Your BMI is: {bmi}</p>}
+      <br />
+      <Link to="/">
+        <button type="button">Go to LandingPage</button>
+      </Link>
+    </div>
+  );
+};
+
+export default BMICalculator;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,5 @@
-import {Component} from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const candidateDetails = {
   firstName: 'Prem Rathan',
@@ -13,43 +14,31 @@ const candidateDetails = {
     'NodeJS',
     'Bootstrap',
   ],
-}
+};
 
-class LandingPage extends Component {
-  render() {
-    const downloadResume = () => {
-      window.open(
-        'https://drive.google.com/file/d/1m0rrSsW7LyuSVQj75ozSJkax5lc_XPB9/view',
-      )
-    }
+const LandingPage = () => {
+  const downloadResume = () => {
+    window.open(
+      'https://drive.google.com/file/d/1m0rrSsW7LyuSVQj75ozSJkax5lc_XPB9/view'
+    );
+  };
 
-    return (
-      <div style={{padding: '10px'}}>
-        <h1>First Name: {candidateDetails.firstName}</h1>
-        <p>
-          Highest Degree Passout Year:{' '}
-          {candidateDetails.highestDegreePassoutYear}
-        </p>
-        <p>
-          Technologies/Languages:{' '}
-          {candidateDetails.technologiesWorkedWith.join(', ')}
-        </p>
-        <button type="button" onClick={downloadResume}>
-          Download Resume
-        </button>
-        <p>
-          Click here to Calculate your BMI:{' '}
-          <button
-            type="button"
-            onClick={() => {
-              window.location.href = '/bmi-calculator'
-            }}
-          >
-            BMI Calculator
-          </button>
-        </p>
-      </div>
-    )
-  }
-}
-export default LandingPage
+  return (
+    <div style={{ padding: '10px' }}>
+      <h1>First Name: {candidateDetails.firstName}</h1>
+      <p>Highest Degree Passout Year: {candidateDetails.highestDegreePassoutYear}</p>
+      <p>Technologies/Languages: {candidateDetails.technologiesWorkedWith.join(', ')}</p>
+      <button type="button" onClick={downloadResume}>
+        Download Resume
+      </button>
+      <p>
+        Click here to Calculate your BMI:{' '}
+        <Link to="/bmi-calculator">
+          <button type="button">BMI Calculator</button>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default LandingPage;
